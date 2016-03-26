4eecaa81925e9e7daadc49312c557937ce5faa5c
Samuele Turci <samuele.turci13@gmail.com>
2016-03-26 12:40:09 +0100

    New XmlNode class to parse xml + Stuff.

--- Divvvv/Divvvv/Stuff.cs
+++ Divvvv/Divvvv/Stuff.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -7,6 +11,24 @@ namespace Divvvv
 {
     public static class Stuff
     {
+        public static TSource MaxBy<TSource>(this IEnumerable<TSource> source, Func<TSource, IComparable> selector)
+        {
+            TSource max = default(TSource);
+            foreach (TSource i in source)
+                if (selector(i).CompareTo(max) > 0)
+                    max = i;
+            return max;
+        }
+
+        public static T? ParseOrNull<T>(this string s) where T : struct
+        {
+            T n = default(T);
+            if ((bool?) typeof (T).GetMethod("TryParse", new[] {typeof (string), typeof (T)})?.Invoke(null, new object[] {s, n}) ?? false)
+                return null;
+            return n;
+
+        }
+
         public static bool IsInt(this string s)
         {
             int n;
@@ -17,7 +39,12 @@ namespace Divvvv
 
         public static string Unescape(this string s) => Regex.Unescape(s);
 
+        public static string NullIfEmpty(this string s) => s == "" ? null : s;
+
         public static string ReMatch(this string s, string re, int groupIdx = 1) => Regex.Match(s, re).Groups[groupIdx].Value;
+
+        public static IEnumerable<string> ReMatches(this string s, string re, int groupIdx = 1) => 
+            Regex.Matches(s, re).Cast<Match>().Select(m => m.Groups[groupIdx].Value);
 
         public static string JsonValue(string json, string key) => json.ReMatch($"\"{key}\":" + "(\")?(.*?)(?(1)\")[,}]", 2);
 
@@ -75,5 +102,29 @@ namespace Divvvv
         public string Value(string key) => Stuff.JsonValue(_json, key);
 
         public static implicit operator Json(string s) => new Json(s);
+    }
+
+    // just because
+    public class XmlNode
+    {
+        private readonly string _xml;
+        private string _innerString = null;
+        public XmlNode(string s)
+        {
+            _xml = s;
+            Tag = s.ReMatch("<([^? ]+?)[ >]");
+        }
+
+        public string Tag { get; }
+
+        public string InnerString => 
+            _innerString ?? (_innerString = _xml.ReMatch($@"<{Tag}.*?>\s*([\s\S]*?)\s*</{Tag}>"));
+
+        public IEnumerable<XmlNode> Nodes => 
+            InnerString.ReMatches(@"<([^? ]+).*?>[\s\S]*?</\1>", 0).Select(s => new XmlNode(s));  
+
+        public string this[string attribute] => _xml.ReMatch($"<{Tag}.*?{attribute}=\"(.*?)\".*?>");
+
+        public static implicit operator XmlNode(string s) => new XmlNode(s);
     }
 }
